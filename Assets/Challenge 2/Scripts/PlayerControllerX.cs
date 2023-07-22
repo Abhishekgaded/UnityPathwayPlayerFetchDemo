065591289851eb818e458373ab4f499c16ed17b3
Abhishekgaded <gadedabhishek01@gmail.com>
2023-07-22 23:10:57 +0530

    Changes applied are =
     - Dogs are Spawning from the Player
     - Different Balls are Spawning from
        the  top of the Screen
     - Balls and the Dogs are Destroying
        after they get into the Contact with
        each other.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeInterval;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInterval = Random.Range(3, 5);
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) )
+        {
+            
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
